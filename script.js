7d53d8aabb597cfd12f1425c23993396fcab9977
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-19 15:25:35 +0530

    commit

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let promise1=new Promise((resolve,reject)=>{
     const randomNumber= Math.random() * (4-1)
     //let tr=document.createElement('tr')
     setTimeout(() => {
-        resolve(`<tr><td>Promise 1</td>time taken by it to resolve in seconds${randomNumber}, for<td></td></tr>`)
+        // resolve(`<tr><td>Promise 1</td>time taken by it to resolve in seconds${randomNumber}, for<td></td></tr>`)
+        resolve(`<tr><td>Loading...</td><td>Loading...</td></tr>`)
     }, randomNumber);
     count+=randomNumber
 })
@@ -17,7 +18,8 @@ let promise2=new Promise((resolve,reject)=>{
     const randomNumber= Math.random() * (4-1)
    // let tr=document.createElement('tr')
     setTimeout(() => {
-        resolve(`<tr><td>Promise 2</td>time taken by it to resolve in seconds${randomNumber}, for<td></td></tr>`)
+        // resolve(`<tr><td>Promise 2</td>time taken by it to resolve in seconds${randomNumber}, for<td></td></tr>`)
+        resolve(`<tr><td>Loading...</td><td>Loading...</td></tr>`)
     }, randomNumber);
     count+=randomNumber
 })
@@ -25,7 +27,8 @@ let promise3=new Promise((resolve,reject)=>{
     const randomNumber= Math.random() * (4-1)
     //let tr=document.createElement('tr')
     setTimeout(() => {
-        resolve(`<tr><td>Promise 2</td>time taken by it to resolve in seconds${randomNumber}, for<td></td></tr>`)
+        // resolve(`<tr><td>Promise 2</td>time taken by it to resolve in seconds${randomNumber}, for<td></td></tr>`)
+        resolve(`<tr><td>Loading...</td><td>Loading...</td></tr>`)
     }, randomNumber);
     count+=randomNumber
 })
@@ -33,7 +36,8 @@ let promise3=new Promise((resolve,reject)=>{
 let promise4=new Promise((resolve,reject)=>{
     let tr=document.createElement('tr')
     setTimeout(() => {
-        resolve(`<tr><td>total</td>time taken to resolve all promises in seconds (should be a decimal number), for ${count}, for<td></td></tr>`)
+        // resolve(`<tr><td>total</td>time taken to resolve all promises in seconds (should be a decimal number), for ${count}, for<td></td></tr>`)
+        resolve(`<tr><td>Loading...</td><td>Loading...</td></tr>`)
     }, count);
 })
 
